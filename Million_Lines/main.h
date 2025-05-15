44df71bd8487fceb8faf3412055e989996c3c587
Ali Diaa <85357930+AliDiaa0@users.noreply.github.com>
2025-05-15 12:59:55 +0300

    Add files via upload

---
+++ Million_Lines/main.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+extern FILE* hea; extern int header;
+extern FILE* fp; extern int ret;
+extern int i;
+extern char path[1000]; extern int x; extern char cmd[1100]; extern char hpath[1100];
+extern char cpath[1100];
+
+int million_code();
